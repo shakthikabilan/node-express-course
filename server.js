b3f5982a7350c1759e0d13835d946b4d40d8582f
Kabi <68263918+shakthikabilan@users.noreply.github.com>
2020-07-20 01:47:54 +0800

    add login POST route

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app =express();
+const bodyParsar = require('body-parser');
 
+app.use(bodyParsar.json())
 
-const mockUserData=[
-{name:'KAbi'},
-{name:'dhenar'}
-]
+const mockUserData=[{name:'KAbi'},{name:'dhenar'}]
 
-app.get('/users',function (req,res)
-{
+app.get('/users',function (req,res){
     res.json({
         success:true,
         message:'succesfully got users.nice!',
@@ -16,15 +14,42 @@ app.get('/users',function (req,res)
     })
 })
 
-app.get('/users/:id',function (req,res)
-{
+//colons are used as variables that be viewed in the params
+
+app.get('/users/:id',function (req,res){
     console.log(req.params.id)
     res.json({
         success:true,
-        message:'succesfully got users.nice!',
-        users:req.params.id
+        message:'got one user!',
+        user: req.params.id
     })
 })
+
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+// it comes from database
+    const mockUsername="Kabilan";
+    const mockPassword="baba";
+
+    if (username===mockUsername && password===mockPassword){
+        //in practice , use json web token sign method here to make an encrypted token
+        res.json({
+            success:true,
+            message:'password and username match',
+            token:'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message:'password and username do not match'
+        })
+    }
+
+})
+
+
 app.listen(8000,function(){
-    console.log("server is running")
+    console.log("server is listening")
 })
